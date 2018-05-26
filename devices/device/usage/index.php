2b19c38382fe7c4654fe46e3465394179647e3c5
Jayasanka Weerasinghe <jayasanka.sack@gmail.com>
2018-05-26 20:06:04 +0530

    get all usage data

---
+++ devices/device/usage/index.php
@@ -0,0 +1,23 @@
+<?php
+include "../../../config/connection.php";
+if (!isset($_GET['id'])) {
+    echo '{
+   "error": {
+      "message": "ID is missing",
+      "type": "InvalidRequestException",
+      "code": 104
+   }
+}';
+    http_response_code(400);
+    exit();
+}
+http_response_code(200);
+$deviceId = $_GET['id'];
+$data = array();
+$usages = array();
+$result = $con->query("SELECT time,data FROM `usage` WHERE deviceId='$deviceId'");
+while ($r = mysqli_fetch_assoc($result)){
+    $usages[] = $r;
+}
+$data["usage_data"] = $usages;
+echo json_encode($data,JSON_PRETTY_PRINT);
