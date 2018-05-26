1a6cc7101f721b8386805d9e089fa803ad88a3ea
Jayasanka Weerasinghe <jayasanka.sack@gmail.com>
2018-05-26 16:25:04 +0530

    devices/device completed

---
+++ devices/device/index.php
@@ -0,0 +1,25 @@
+<?php
+include "../../config/connection.php";
+if(!isset($_GET['id'])){
+    echo '{
+   "error": {
+      "message": "ID is missiong",
+      "type": "InvalidRequestException",
+      "code": 104
+   }
+}';
+    http_response_code(400);
+    exit();
+}
+$deviceId = $_GET['id'];
+$res = $con->query("SELECT * FROM device WHERE deviceId='$deviceId'");
+$data = array();
+if(mysqli_num_rows($res)==0){
+    http_response_code(204);
+}else{
+    while ($device = mysqli_fetch_assoc($res)){
+        $data = $device;
+    }
+    http_response_code(200);
+    echo json_encode($data,JSON_PRETTY_PRINT);
+}
---
+++ devices/index.php
@@ -0,0 +1,11 @@
+<?php
+include "../config/connection.php";
+$res = $con->query("SELECT * FROM device");
+$data = array();
+$devices = array();
+while ($device = mysqli_fetch_assoc($res)){
+    $devices[] = $device;
+}
+http_response_code(200);
+$data["devices"] = $devices;
+echo json_encode($data,JSON_PRETTY_PRINT);
