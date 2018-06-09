860aa5c2dd91dcc7403837ca8a09f93bf745c2d1
Jayasanka Weerasinghe <jayasanka.sack@gmail.com>
2018-06-09 17:33:22 +0530

    changed data to usage

--- api/v1/resources/devices/device/usage/years/index.php
+++ api/v1/resources/devices/device/usage/years/index.php
@@ -19,11 +19,11 @@ $deviceId = $_GET['id'];
 $result = $con->query("SELECT YEAR(time) AS year, SUM(data) AS data FROM `usage` WHERE deviceId='$deviceId' GROUP BY year");
 while($r = mysqli_fetch_assoc($result)){
     $usage['year'] = $r['year'];
-    $usage['data'] = intval($r['data']);
+    $usage['usage'] = intval($r['data']);
     $usageData[] = $usage;
 }
 $data["id"] = $deviceId;
-$data['usage_data'] = $usageData;
+$data['years'] = $usageData;
 echo json_encode($data,JSON_PRETTY_PRINT);
 
 http_response_code(200);
