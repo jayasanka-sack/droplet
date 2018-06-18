37f3854f9c563538ee82cd6ee0c9e634b8c11eb4
Jayasanka Weerasinghe <jayasanka.sack@gmail.com>
2018-06-18 10:11:19 +0530

    fixed wrong variable name

--- api/v1/config/functions.php
+++ api/v1/config/functions.php
@@ -15,7 +15,7 @@ function sendCurl($url){
 }
 function isValidDate($date) {
     $valid = false;
-    if(preg_match("/^(\d{4})-(\d{2})-(\d{2})$/", $zdate, $matches))
+    if(preg_match("/^(\d{4})-(\d{2})-(\d{2})$/", $date, $matches))
     {
         if(checkdate($matches[2], $matches[3], $matches[1]))
         {
