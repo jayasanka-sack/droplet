1f508f5444908b05e2029ec3846a541cbea446bb
Yohan_Avishke <yaediriweera@gmail.com>
2018-06-20 23:32:27 +0530

    Add files via upload

---
+++ web/Dashboard/index.php
@@ -0,0 +1,276 @@
+<!DOCTYPE html>
+<?php
+$curl = curl_init();
+curl_setopt_array($curl, array(
+    CURLOPT_RETURNTRANSFER => 1,
+    CURLOPT_FOLLOWLOCATION => 1,
+    CURLOPT_URL => 'http://139.59.81.23/apis/droplet/test/fake.php'
+));
+$post = json_decode(curl_exec($curl));
+curl_close($curl);
+
+date_default_timezone_set("Asia/Colombo");
+?>
+<html lang="en">
+<head>
+    <meta charset="utf-8"/>
+    <link rel="icon" type="image/png" href="assets/img/favicon.ico">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"/>
+
+    <title>Light Bootstrap Dashboard by Creative Tim</title>
+
+    <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport'/>
+    <meta name="viewport" content="width=device-width"/>
+
+
+    <!-- Bootstrap core CSS     -->
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet"/>
+
+    <!-- Animation library for notifications   -->
+    <link href="assets/css/animate.min.css" rel="stylesheet"/>
+
+    <!--  Light Bootstrap Table core CSS    -->
+    <link href="assets/css/light-bootstrap-dashboard.css?v=1.4.0" rel="stylesheet"/>
+
+    <!--     Fonts and icons     -->
+    <link href="http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+    <link href='http://fonts.googleapis.com/css?family=Roboto:400,700,300' rel='stylesheet' type='text/css'>
+    <link href="assets/css/pe-icon-7-stroke.css" rel="stylesheet"/>
+
+    <!--firebase-->
+    <script src="https://www.gstatic.com/firebasejs/5.0.3/firebase.js"></script>
+    <script src="https://www.gstatic.com/firebasejs/5.0.1/firebase-app.js"></script>
+    <script src="https://www.gstatic.com/firebasejs/5.0.1/firebase-auth.js"></script>
+    <script>
+        // Initialize Firebase
+        var config = {
+            apiKey: "AIzaSyAia6DXR7EM7JdJ9UEI38piOfqEdnS0KX0",
+            authDomain: "droplet-204301.firebaseapp.com",
+            databaseURL: "https://droplet-204301.firebaseio.com",
+            projectId: "droplet-204301",
+            storageBucket: "droplet-204301.appspot.com",
+            messagingSenderId: "658684640307"
+        };
+        firebase.initializeApp(config);
+    </script>
+</head>
+<body>
+
+<div class="wrapper">
+    <div class="sidebar" data-image="assets/img/sidebar-5.jpg">
+        <!--
+    Tip 1: You can change the color of the sidebar using: data-color="purple | blue | green | orange | red"
+
+    Tip 2: you can also add an image using data-image tag
+-->
+        <div class="sidebar-wrapper">
+            <div class="logo">
+                <a href="http://www.creative-tim.com" class="simple-text">
+                    Creative Tim
+                </a>
+            </div>
+            <ul class="nav">
+                <li class="nav-item active">
+                    <a class="nav-link" href="dashboard.html">
+                        <i class="nc-icon nc-chart-pie-35"></i>
+                        <p>Dashboard</p>
+                    </a>
+                </li>
+                <li>
+                    <a class="nav-link" href="./user.html">
+                        <i class="nc-icon nc-circle-09"></i>
+                        <p>User Profile</p>
+                    </a>
+                </li>
+                <li>
+                    <a class="nav-link" href="./table.html">
+                        <i class="nc-icon nc-notes"></i>
+                        <p>Table List</p>
+                    </a>
+                </li>
+                <li>
+                    <a class="nav-link" href="./typography.html">
+                        <i class="nc-icon nc-paper-2"></i>
+                        <p>Typography</p>
+                    </a>
+                </li>
+                <li>
+                    <a class="nav-link" href="./icons.html">
+                        <i class="nc-icon nc-atom"></i>
+                        <p>Icons</p>
+                    </a>
+                </li>
+                <li>
+                    <a class="nav-link" href="./maps.html">
+                        <i class="nc-icon nc-pin-3"></i>
+                        <p>Maps</p>
+                    </a>
+                </li>
+                <li>
+                    <a class="nav-link" href="./notifications.html">
+                        <i class="nc-icon nc-bell-55"></i>
+                        <p>Notifications</p>
+                    </a>
+                </li>
+                <li class="nav-item active active-pro">
+                    <a class="nav-link active" href="upgrade.html">
+                        <i class="nc-icon nc-alien-33"></i>
+                        <p>Upgrade to PRO</p>
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
+    <div class="main-panel">
+        <nav class="navbar navbar-default navbar-fixed">
+            <div class="container-fluid">
+                <div class="navbar-header">
+                    <a class="navbar-brand" href="#">Droplet</a>
+                </div>
+                <ul class="nav navbar-nav navbar-right">
+                    <li>
+                        <button id="btn-log-out" class="btn btn-danger">Logout</button>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+
+        <div class="content">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-md-4">
+                        <div class="card">
+                            <div class="header">
+                                <h4 class="title">Water level</h4>
+                            </div>
+                            <div class="content">
+                                <div id="chartPreferences" class="ct-chart ct-perfect-fourth"></div>
+                                <div class="footer">
+                                    <div class="legend">
+                                        <i class="fa fa-circle text-info"></i> Remaining
+                                        <i class="fa fa-circle text-danger"></i> Used
+                                    </div>
+                                    <hr>
+                                    <div class="stats">
+                                        <i class="fa fa-clock-o"></i> Updated on <?php echo date('l jS \of F Y') ?> <br>at <?php echo date("h:i:s A") ?>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-md-8">
+                        <div class="card">
+                            <div class="header">
+                                <h4 class="title">Users Behavior</h4>
+                                <p class="category">24 Hours performance</p>
+                            </div>
+                            <div class="content">
+                                <div id="chartHours" class="ct-chart"></div>
+                                <div class="footer">
+                                    <div class="legend">
+                                        <i class="fa fa-circle text-info"></i> Open
+                                        <i class="fa fa-circle text-danger"></i> Click
+                                        <i class="fa fa-circle text-warning"></i> Click Second Time
+                                    </div>
+                                    <hr>
+                                    <div class="stats">
+                                        <i class="fa fa-history"></i> Updated 3 minutes ago
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+</body>
+
+<!--   Core JS Files   -->
+<script src="assets/js/jquery.3.2.1.min.js" type="text/javascript"></script>
+<script src="assets/js/bootstrap.min.js" type="text/javascript"></script>
+
+<!--  Charts Plugin -->
+<script src="assets/js/chartist.min.js"></script>
+
+<!--  Notifications Plugin    -->
+<script src="assets/js/bootstrap-notify.js"></script>
+
+<!-- Light Bootstrap Table Core javascript and methods for Demo purpose -->
+<script src="assets/js/light-bootstrap-dashboard.js?v=1.4.0"></script>
+
+<script type="text/javascript">
+    $(document).ready(function () {
+
+        var dataSales = {
+            labels: ['9:00AM', '12:00AM', '3:00PM', '6:00PM', '9:00PM', '12:00PM', '3:00AM', '6:00AM'],
+            series: [
+                [287, 385, 490, 492, 554, 586, 698, 695, 752, 788, 846, 944],
+                [67, 152, 143, 240, 287, 335, 435, 437, 539, 542, 544, 647],
+                [23, 113, 67, 108, 190, 239, 307, 308, 439, 410, 410, 509]
+            ]
+        };
+
+        var optionsSales = {
+            lineSmooth: false,
+            low: 0,
+            high: 800,
+            showArea: true,
+            height: "245px",
+            axisX: {
+                showGrid: false,
+            },
+            lineSmooth: Chartist.Interpolation.simple({
+                divisor: 3
+            }),
+            showLine: false,
+            showPoint: false,
+        };
+
+        var responsiveSales = [
+            ['screen and (max-width: 640px)', {
+                axisX: {
+                    labelInterpolationFnc: function (value) {
+                        return value[0];
+                    }
+                }
+            }]
+        ];
+
+        Chartist.Line('#chartHours', dataSales, optionsSales, responsiveSales);
+        var dataPreferences = {
+            series: [
+                [25, 30, 20, 25]
+            ]
+        };
+
+        var optionsPreferences = {
+            donut: true,
+            donutWidth: 40,
+            startAngle: 0,
+            total: 100,
+            showLabel: false,
+            axisX: {
+                showGrid: false
+            }
+        };
+        Chartist.Pie('#chartPreferences', dataPreferences, optionsPreferences);
+
+        Chartist.Pie('#chartPreferences', {
+            labels: ['<?php echo $post->level;?>%', '<?php echo 100 - $post->level;?>%'],
+            series: [<?php echo $post->level;?>, <?php echo 100 - $post->level;?>]
+        });
+    });
+</script>
+<script>
+    function userLogOut() {
+        firebase.auth().signOut();
+        window.location.href = '../Web-pages/Pages/login-page.html';
+    }
+    document.getElementById('btn-log-out').addEventListener('click',userLogOut);
+</script>
+</html>
